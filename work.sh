6723f592ef39149855de65f928e8de9b432a619b
Nazarko12 <nazartsiupryk@gmail.com>
2021-10-23 13:28:12 +0200

    A file with alternating commands to run the application was added

---
+++ work.sh
@@ -0,0 +1,19 @@
+sudo apt update
+
+sudo apt install maven
+
+sudo apt install default-jre maven mysql-server 
+
+sudo apt install openjdk-11-jdk
+
+sudo apt install apache2
+
+git clone https://github.com/Nazarko12/examination-task.git
+
+ls -l
+
+cd examination-task/
+
+sudo mvn install
+
+sudo java -jar target/rest-0.0.1-SNAPSHOT.jar
